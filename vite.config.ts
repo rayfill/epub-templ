67311ae8230dd0c81eff9b594c757f395f506f3b
rayfill <rayfill@gmail.com>
2025-02-01 01:23:17 +0900

    fix package volnerability

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
 import { UserConfig, defineConfig } from 'vite';
 import { resolve } from 'node:path';
-import checker from 'vite-plugin-checker';
-import dts from 'vite-plugin-dts';
 
 // import banner, { BannerPluginOptions } from 'vite-plugin-banner';
 // const bannerPluginOptions: BannerPluginOptions = {
@@ -25,8 +23,6 @@ const userConfig: UserConfig = {
     }
   },
   plugins: [
-    checker({ typescript: true }),
-    dts(),
     //banner(bannerPluginOptions),
   ],
 };
